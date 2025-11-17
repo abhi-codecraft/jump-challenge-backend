5709a93c04a9317138e35fc4539f509f8c79047d
Abhijith <abhicodecraft@gmail.com>
2025-11-17 12:01:31 +0530

    google redirect url added to env

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -10,7 +10,7 @@ export const googleStrategySetup = () => {
             {
                 clientID: process.env.GOOGLE_CLIENT_ID,
                 clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                callbackURL: "http://localhost:3000/auth/google/callback",
+                callbackURL: process.env.GOOGLE_REDIRECT_URL,
                 scope: [
                     "profile",
                     "email",
